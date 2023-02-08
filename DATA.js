ffbdb4346e91d71cde90433f848445026d54e95f
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-08 18:57:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,49 @@
+export const item = {
+    "id": "8d8d33cf-d5c7-46f0-be9b-e667a67f703b",
+    "primer_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png",
+    "skill_title": "Recent guy food according commercial around. Because book trade field throughout scene. Property experience air staff.",
+    "suggestion": "Former ball prepare. Color interview list necessary himself child.",
+    "featured_title": "Article spend still across always former there four. Magazine whole including. If suffer on discover player before relationship.",
+    "lessons": [
+        {
+            "id": "7431524b-da0a-48ed-95f1-c41b0b05bb99",
+            "lesson_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png",
+            "about_lesson": "That expect television present weight. Rock good nearly worker feel just meet boy. Marriage race measure which.",
+            "by_text": "Single affect must dream standard program. Toward guy sister through involve. Loss lawyer class score weight ahead.",
+            "lesson_type_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png",
+            "lesson_type": "Least should quite concern beautiful prove key. Woman common material voice traditional use the. Gun scene share lawyer water health."
+        },
+        {
+            "id": "6d6bb53a-c9bf-46fd-9ffd-ff817b33831f",
+            "lesson_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png",
+            "about_lesson": "Southern thought perhaps game owner get. Reality color board ready vote main. Soon southern cut poor.",
+            "by_text": "Language where thank stay capital. Discover expert ahead hot particularly fill.",
+            "lesson_type_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/5.png",
+            "lesson_type": "Class receive much man hit. Can minute campaign commercial difficult add."
+        },
+        {
+            "id": "f8400812-e6a8-4eb6-83da-50e676e2f4bc",
+            "lesson_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png",
+            "about_lesson": "His treatment field rule current respond. Part rule break know sense join stock.",
+            "by_text": "Paper hit spend raise certainly interview. Range modern nor cover environmental success but.",
+            "lesson_type_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/2.png",
+            "lesson_type": "Store wind left plant last day current. Most great effect law avoid. Know ten paper born explain different."
+        },
+        {
+            "id": "7592d39e-2bff-4dd4-88a9-e273a7ac2e02",
+            "lesson_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png",
+            "about_lesson": "Democrat about large. Nice student its civil pretty environmental. Increase picture rather enough same some.",
+            "by_text": "Not country bar call Congress role analysis. Rate company true wear he leader lose.",
+            "lesson_type_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png",
+            "lesson_type": "Carry will sing commercial necessary if consider. Candidate marriage individual let draw experience including listen."
+        },
+        {
+            "id": "fe29a0dd-5d5d-47da-a15a-6543c69aa7d7",
+            "lesson_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png",
+            "about_lesson": "Physical large north throw determine senior when. Parent check public college type customer low.",
+            "by_text": "Fall from through run. Understand benefit second east policy amount among. Herself sit do water ground almost somebody fear.",
+            "lesson_type_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png",
+            "lesson_type": "Create interview through international head recent. Stuff usually over both head once Mrs."
+        }
+    ]
+}
